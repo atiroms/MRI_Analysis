4a95faece92eaca002490ad10b7b29f504352e84
atiroms <atirom.umusus@gmail.com>
2018-11-08 22:25:11 +0900

    pickupfile added

--- Utilities/FolderStructure.py
+++ Utilities/FolderStructure.py
@@ -1,12 +1,17 @@
 
 
-path = '/media/atiroms/MORITA_HDD4/MRI/COCORO/Analysis/FunImg'
+#path = '/media/atiroms/MORITA_HDD4/MRI/COCORO/Analysis/FunImg'
 #path = '/media/atiroms/MORITA_HDD4/MRI/COCORO/test2/FunImg'
+path='/media/atiroms/MORITA_HDD4/MRI/COCORO/test2/Results/FC_FunImgARWSCF'
+
+rois = [37, 38, 41, 42, 71, 72, 73, 74, 75, 76, 77, 78]
+prefix = 'ROI'
 
 suffix = '.nii.gz'
 
 import os
 import shutil
+import glob
 
 # copy file name to folder name and put file into folder
 # used for dparsf
@@ -24,4 +29,18 @@ class FileIntoFolder():
                     os.makedirs(path_newfolder)
                 shutil.move(path_oldfile, path_newfolder)
                 print('Moved ' + f + '.')
-            
+
+
+class PickupFile():
+    def __init__(self,path=path,prefix=prefix,rois=rois):
+        self.path=path
+        self.prefix=prefix
+        for roi in rois:
+            list_file=glob.glob(self.path + '/ROI' + str(roi) + 'FC*.nii')
+            path_tofolder = self.path + '/ROI' + str(roi)
+            if not os.path.exists(path_tofolder):
+                os.makedirs(path_tofolder)
+            for f in list_file:
+                shutil.copy(f,path_tofolder)
+            print('Picked ROI ' + str(roi) + ' files.')                
+
