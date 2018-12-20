41ced201a4de451b7cf60fefc67df41de8fe5314
atiroms <atirom.umusus@gmail.com>
2018-12-20 20:30:09 +0900

    fMRIPrep no surface analysis version added

--- BIDS_Scripts/fMRIPrep.sh
+++ BIDS_Scripts/fMRIPrep.sh
@@ -3,7 +3,8 @@ sudo docker run -ti --rm -v /media/veracrypt1/MRI/pnTTC/BIDS/test_3sub/Nifti3_fM
 
 
 ##
- fmriprep-docker --fs-license-file /usr/local/freesurfer/license.txt /media/veracrypt1/MRI/pnTTC/BIDS/test_3sub/Nifti3_fMRIPrep /media/veracrypt1/MRI/pnTTC/BIDS/test_3sub/fMRIPrep_output
+fmriprep-docker --fs-license-file /usr/local/freesurfer/license.txt /media/veracrypt1/MRI/pnTTC/BIDS/test_3sub/Nifti3_fMRIPrep /media/veracrypt1/MRI/pnTTC/BIDS/test_3sub/fMRIPrep_output
 
-##
- fmriprep-docker --fs-license-file /usr/local/freesurfer/license.txt --notrack /media/veracrypt1/MRI/pnTTC/BIDS/test_3sub/Nifti3_fMRIPrep /media/veracrypt1/MRI/pnTTC/BIDS/test_3sub/fMRIPrep_output
+
+# no tracking and no surface analysis version
+fmriprep-docker --fs-license-file /usr/local/freesurfer/license.txt --notrack --fs-no-reconall /media/veracrypt1/MRI/pnTTC/BIDS/test_3sub/Nifti5_fMRIPrep_nosurf /media/veracrypt1/MRI/pnTTC/BIDS/test_3sub/fMRIPrep_nosurf_output
