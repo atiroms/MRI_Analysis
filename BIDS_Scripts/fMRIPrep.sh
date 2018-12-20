692ae0d5033673077ba034efa44847601778d09f
atiroms <atirom.umusus@gmail.com>
2018-12-20 18:55:01 +0900

    fMRIPrep and MRIQC scripts initialized

---
+++ BIDS_Scripts/fMRIPrep.sh
@@ -0,0 +1,9 @@
+sudo docker run -ti --rm -v /media/veracrypt1/MRI/pnTTC/BIDS/test_3sub/Nifti3_fMRIPrep:/data:ro -v /media/veracrypt1/MRI/pnTTC/BIDS/test_3sub/fMRIPrep_output:/out poldracklab/fmriprep:latest /data /out/out participant --fs-license-file /usr/local/freesurfer/license.txt
+
+
+
+##
+ fmriprep-docker --fs-license-file /usr/local/freesurfer/license.txt /media/veracrypt1/MRI/pnTTC/BIDS/test_3sub/Nifti3_fMRIPrep /media/veracrypt1/MRI/pnTTC/BIDS/test_3sub/fMRIPrep_output
+
+##
+ fmriprep-docker --fs-license-file /usr/local/freesurfer/license.txt --notrack /media/veracrypt1/MRI/pnTTC/BIDS/test_3sub/Nifti3_fMRIPrep /media/veracrypt1/MRI/pnTTC/BIDS/test_3sub/fMRIPrep_output
