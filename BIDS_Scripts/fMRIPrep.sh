fa25cddce3cb33bf413e6ca716057d29e24af4b3
atiroms <atirom.umusus@gmail.com>
2018-12-25 16:36:13 +0900

    small changes

--- BIDS_Scripts/fMRIPrep.sh
+++ BIDS_Scripts/fMRIPrep.sh
@@ -22,3 +22,7 @@ fmriprep-docker --fs-license-file /usr/local/freesurfer/license.txt --notrack --
 
 ## 
 fmriprep-docker --fs-license-file /usr/local/freesurfer/license.txt --notrack --template-resampling-grid '2mm' --write-graph --use-aroma /media/veracrypt1/MRI/pnTTC/BIDS/03_ses1 /media/veracrypt1/MRI/pnTTC/BIDS/04_fmriprep
+
+
+## use SyN for susceptibility distortion correction when Fieldmap data is not available
+fmriprep-docker --fs-license-file /usr/local/freesurfer/license.txt --notrack --template-resampling-grid '2mm' --write-graph --use-aroma --use-syn-sdc /media/veracrypt1/MRI/pnTTC/BIDS/test_1sub/04_slicetiming_1ses /media/veracrypt1/MRI/pnTTC/BIDS/test_1sub/07_fmriprep_syn_1ses
