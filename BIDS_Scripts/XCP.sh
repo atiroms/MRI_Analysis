55fa8a658accdc977d6c19784445b748247f890c
atiroms <atirom.umusus@gmail.com>
2019-01-07 21:26:55 +0900

    xcp script initialized

---
+++ BIDS_Scripts/XCP.sh
@@ -0,0 +1,5 @@
+## test anatomical analysis on 1 subject
+singularity run -B /media/veracrypt1/MRI/pnTTC/BIDS/test_1sub/15_xcp_anat:${HOME}/data /data/applications/xcpEngine.simg -d ${HOME}/data/anat-antsct.dsn -c ${HOME}/data/anat_cohort.csv -o ${HOME}/data/xcp_output -t 1 -r ${HOME}/data
+
+## test functional analysis on 1 subject
+singularity run -B /media/veracrypt1/MRI/pnTTC/BIDS/test_1sub/16_xcp_func:${HOME}/data /data/applications/xcpEngine.simg -d ${HOME}/data/fc-36p.dsn -c ${HOME}/data/func_cohort.csv -o ${HOME}/data/xcp_output -t 1 -r ${HOME}/data
