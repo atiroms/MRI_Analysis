ce6bfb549214e44eb4ae20ed7fd453f31eba2d31
atiroms <atirom.umusus@gmail.com>
2019-01-08 20:52:18 +0900

    create cohort updated

--- BIDS_Scripts/XCP.sh
+++ BIDS_Scripts/XCP.sh
@@ -9,3 +9,15 @@ singularity run -B /media/veracrypt1/MRI/pnTTC/BIDS/test_1sub/15_xcp:${HOME}/dat
 
 ## test anatomical and functional analysis on 1 subject and send email (tutorial dataset)
 singularity run -B /media/veracrypt1/MRI/XCP_tutorial:${HOME}/data /data/applications/xcpEngine.simg -d ${HOME}/data/anat-antsct.dsn -c ${HOME}/data/anat_cohort.csv -o ${HOME}/data/xcp_output -t 1 -r ${HOME}/data && singularity run -B /media/veracrypt1/MRI/XCP_tutorial:${HOME}/data /data/applications/xcpEngine.simg -d ${HOME}/data/fc-36p.dsn -c ${HOME}/data/func_cohort.csv -o ${HOME}/data/xcp_output -t 1 -r ${HOME}/data && echo -e "Subject: Automatic Notification\n\nAnat and Func analysis on tutorial dataset done" | sendmail atirom.umusus@gmail.com
+
+## functional-only analysis on 1 subject and send email (original dataset)
+singularity run -B /media/veracrypt1/MRI/pnTTC/BIDS/test_1sub/16_xcp:${HOME}/data /data/applications/xcpEngine.simg -d ${HOME}/data/fc-36p_dvo.dsn -c ${HOME}/data/func_cohort.csv -o ${HOME}/data/xcp_output -t 1 -r ${HOME}/data && echo -e "Subject: Automatic Notification\n\nFunc-only analysis on original dataset of 1 sub done" | sendmail atirom.umusus@gmail.com
+
+## functional-only analysis on 1 subject and send email (original dataset) (re-run with /anat contents copied to session-specific directory) * error with unmatched volume number, probably caused by volume deletion
+singularity run -B /media/veracrypt1/MRI/pnTTC/BIDS/test_1sub/17_xcp:${HOME}/data /data/applications/xcpEngine.simg -d ${HOME}/data/fc-36p_dvo.dsn -c ${HOME}/data/func_cohort.csv -o ${HOME}/data/xcp_output -t 1 -r ${HOME}/data && echo -e "Subject: Automatic Notification\n\nFunc-only analysis on original dataset of 1 sub done" | sendmail atirom.umusus@gmail.com
+
+## functional-only analysis on 1 subject and send email (original dataset) (re-run with /anat contents copied to session-specific directory)
+singularity run -B /media/veracrypt1/MRI/pnTTC/BIDS/test_1sub/18_xcp:${HOME}/data /data/applications/xcpEngine.simg -d ${HOME}/data/fc-36p.dsn -c ${HOME}/data/func_cohort.csv -o ${HOME}/data/xcp_output -t 1 -r ${HOME}/data && echo -e "Subject: Automatic Notification\n\nFunc-only analysis on original dataset of 1 sub done" | sendmail atirom.umusus@gmail.com
+
+## functional-only analysis on 1 subject and send email (original dataset) (re-run with /anat contents copied to session-specific directory) * only coregistration metrics copied *error
+singularity run -B /media/veracrypt1/MRI/pnTTC/BIDS/test_1sub/19_xcp:${HOME}/data /data/applications/xcpEngine.simg -d ${HOME}/data/fc-36p.dsn -c ${HOME}/data/func_cohort.csv -o ${HOME}/data/xcp_output -t 1 -r ${HOME}/data && echo -e "Subject: Automatic Notification\n\nFunc-only analysis on original dataset of 1 sub done" | sendmail atirom.umusus@gmail.com
--- BIDS_Scripts/XCP_prepare.py
+++ BIDS_Scripts/XCP_prepare.py
@@ -9,11 +9,13 @@ import pandas as pd
 #import json
 
 
-########################
-# anat_cohort.csv file #
-########################
-path_out='C:/Users/atiro/Dropbox/MRI/XCP_tutorial'
-path_file_id='C:/Users/atiro/Dropbox/MRI/XCP_tutorial/id.txt'
+###############
+# COHORT FILE #
+###############
+#path_out='C:/Users/atiro/Dropbox/MRI/XCP_tutorial'
+#path_file_id='C:/Users/atiro/Dropbox/MRI/XCP_tutorial/id.txt'
+path_out='/media/veracrypt1/MRI/pnTTC/BIDS/test_5sub/12_xcp'
+path_file_id='/media/veracrypt1/MRI/pnTTC/BIDS/test_5sub/12_xcp/id.txt'
 
 class CreateCohortfile():
     def __init__(self,path_out=path_out,path_file_id=path_file_id):
@@ -21,17 +23,23 @@ class CreateCohortfile():
             list_id=list_id.readlines()
             list_id=[x.strip('\n') for x in list_id]
         output_anat=pd.DataFrame(columns=['id0','img'])
-        output_func=pd.DataFrame(columns=['id0','antsct','img'])
+        #output_func=pd.DataFrame(columns=['id0','antsct','img'])
+        output_func=pd.DataFrame(columns=['id0','img'])
         for index in list_id:
             output_anat=output_anat.append(pd.Series(['sub-'+str(index).zfill(5),
                                                       'fmriprep/sub-'+str(index).zfill(5)+'/anat/sub-'+str(index).zfill(5)+'_desc-preproc_T1w.nii.gz'],
                                                      index=output_anat.columns),
                                            ignore_index=True)
             output_func=output_func.append(pd.Series(['sub-'+str(index).zfill(5),
-                                                      'xcp_output/sub-'+str(index).zfill(5)+'/struc',
-                                                      'fmriprep/sub-'+str(index).zfill(5)+'/func/sub-'+str(index).zfill(5)+'_task-rest_space-T1w_desc-preproc_bold.nii.gz'],
+                                                      #'xcp_output/sub-'+str(index).zfill(5)+'/struc',
+                                                      '10_remini_syn_12dof/fmriprep/sub-'+str(index).zfill(5)+'/func/sub-'+str(index).zfill(5)+'_ses-01_task-rest_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz'],
                                                      index=output_func.columns),
                                            ignore_index=True)
-        output_anat.to_csv(os.path.join(path_out,'anat_cohort.csv'),index=False)
+        #output_anat.to_csv(os.path.join(path_out,'anat_cohort.csv'),index=False)
         output_func.to_csv(os.path.join(path_out,'func_cohort.csv'),index=False)
         print('All done.')
+
+
+####################
+# COPY ANAT FOLDER #
+####################
