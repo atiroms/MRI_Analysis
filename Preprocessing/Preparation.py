da27327358833ec2419b302a3a70b2c2dc614bf5
atiroms <atirom.umusus@gmail.com>
2019-01-30 22:13:38 +0900

    extract DICOM header metadata

--- Preprocessing/Preparation.py
+++ Preprocessing/Preparation.py
@@ -9,6 +9,8 @@ import csv
 import nilearn.image as nl_image
 import json
 import numpy as np
+import pydicom
+
 
 ##################################################
 # Insert slice timing and phase encoding direction
@@ -275,6 +277,52 @@ class SpaceIDFile():
         cols_df=['ID_pnTTC'] +cols_df
         df=df[cols_df]
         df.to_csv(path_file_output,sep='\t',index=False)
+        print('All done.')
+
+
+##################################################
+# Extract DICOM header metadata
+##################################################
+# used to extract scan date information
+
+class ExtractDcmHeader():
+    def __init__(self,
+        path_file_in='P:/MRI/pnTTC/Preproc/00_dicom_ses12_exist/pnTTC2_T1/CSUB-00003C-02/IM-0001-0001-0001.dcm'
+        ):
+
+        img_in=pydicom.read_file(path_file_in)
+        df_header=pd.DataFrame(columns=['tag','name','value'])
+        for element in img_in:
+            df_header=df_header.append(pd.Series([str(element.tag),element.name,element.repval],index=df_header.columns),ignore_index=True)
+        self.output=df_header
+        #print('DICOM element count: '+ str(len(df_header)))
+        #print('All done.')
+
+class ExtractMltDcmHeader():
+    def __init__(self,
+        path_exp='P:/MRI/pnTTC/Preproc/test_5sub/27_dicom_ses2_t1w/output',
+        path_file_output='P:/MRI/pnTTC/Preproc/test_5sub/28_header_date_ses2_t1w/output/dcmheader.csv',
+        keys=['Acquisition Date'],
+        prefix_dir_sub='CSUB-',
+        suffix_dir_sub='C-02'
+        ):
+
+        list_dir_sub = os.listdir(path_exp)
+        list_dir_sub.sort()
+        df_header_mlt=pd.DataFrame(columns=['ID_pnTTC','dir_sub','key','value'])
+        for dir_sub in list_dir_sub:
+            ID_pnTTC=int(dir_sub.replace(prefix_dir_sub,'').replace(suffix_dir_sub,''))
+            list_img=os.listdir(os.path.join(path_exp,dir_sub))
+            list_img.sort()
+            path_firstimg=os.path.join(path_exp,dir_sub,list_img[0])
+            df_header_firstimg=ExtractDcmHeader(path_firstimg).output
+            for key in keys:
+                value=df_header_firstimg.loc[df_header_firstimg['name']==key,'value'].values[0]
+                df_header_mlt=df_header_mlt.append(pd.Series([ID_pnTTC,dir_sub,key,value],index=df_header_mlt.columns),ignore_index=True)
+            print('Checked directory ' + dir_sub + '.')
+        
+        self.output=df_header_mlt
+        df_header_mlt.to_csv(path_file_output,index=False)
         print('All done.')
 
 
