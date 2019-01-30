b3d17563405dd9613ee1e577c970cbc368440060
atiroms <atirom.umusus@gmail.com>
2019-01-30 23:05:46 +0900

    small changes

--- Preprocessing/Preparation.py
+++ Preprocessing/Preparation.py
@@ -300,11 +300,16 @@ class ExtractDcmHeader():
 
 class ExtractMltDcmHeader():
     def __init__(self,
-        path_exp='P:/MRI/pnTTC/Preproc/test_5sub/27_dicom_ses2_t1w/output',
-        path_file_output='P:/MRI/pnTTC/Preproc/test_5sub/28_header_date_ses2_t1w/output/dcmheader.csv',
+        #path_exp='P:/MRI/pnTTC/Preproc/test_5sub/27_dicom_ses2_t1w/output',
+        path_exp='P:/MRI/pnTTC/Preproc/00_dicom_ses12_exist/pnTTC1_T1',
+        #path_exp='P:/MRI/pnTTC/Preproc/00_dicom_ses12_exist/pnTTC2_T1',
+        #path_file_output='P:/MRI/pnTTC/Preproc/test_5sub/28_header_date_ses2_t1w/output/dcmheader.csv',
+        path_file_output='P:/MRI/pnTTC/Preproc/18_acquisitiondate_t1w/output/dcmheader_ses1.csv',
+        #path_file_output='P:/MRI/pnTTC/Preproc/18_acquisitiondate_t1w/output/dcmheader_ses2.csv',
         keys=['Acquisition Date'],
         prefix_dir_sub='CSUB-',
-        suffix_dir_sub='C-02'
+        suffix_dir_sub='C-01'
+        #suffix_dir_sub='C-02'
         ):
 
         list_dir_sub = os.listdir(path_exp)
@@ -313,6 +318,7 @@ class ExtractMltDcmHeader():
         for dir_sub in list_dir_sub:
             ID_pnTTC=int(dir_sub.replace(prefix_dir_sub,'').replace(suffix_dir_sub,''))
             list_img=os.listdir(os.path.join(path_exp,dir_sub))
+            list_img=[img for img in list_img if img.startswith('IM-')]
             list_img.sort()
             path_firstimg=os.path.join(path_exp,dir_sub,list_img[0])
             df_header_firstimg=ExtractDcmHeader(path_firstimg).output
@@ -321,8 +327,19 @@ class ExtractMltDcmHeader():
                 df_header_mlt=df_header_mlt.append(pd.Series([ID_pnTTC,dir_sub,key,value],index=df_header_mlt.columns),ignore_index=True)
             print('Checked directory ' + dir_sub + '.')
         
-        self.output=df_header_mlt
+        #df_header_mlt.to_csv(path_file_output,index=False)
+
+        list_id_input=list(df_header_mlt['ID_pnTTC'])
+        list_id_output=list(range(1,max(list_id_input)+1,1))
+        list_id_diff=list(set(list_id_output)-set(list_id_input))
+        list_id_diff.sort()
+        df_space=pd.DataFrame(np.nan,columns=df_header_mlt.columns,index=range(len(list_id_diff)))
+        df_space['ID_pnTTC']=list_id_diff
+        df_header_mlt=pd.concat([df_header_mlt,df_space])
+        df_header_mlt=df_header_mlt.sort_values(by='ID_pnTTC')
+
         df_header_mlt.to_csv(path_file_output,index=False)
+
         print('All done.')
 
 
