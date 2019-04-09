8bc7f6095da300b88d92266b556a643d150ebd46
atiroms <atirom.umusus@gmail.com>
2019-04-10 08:47:00 +0900

    small changes

--- preproc/prep_fmriprep.py
+++ preproc/prep_fmriprep.py
@@ -342,23 +342,23 @@ class PrepFmriprep():
         #path_bids='C:/Users/atiro/Dropbox/Temp/Preproc/test_1sub/32_heudiconv',
         #path_bids='/media/veracrypt1/MRI/pnTTC/Preproc/test_5sub/53_bids_fmap',
         #path_bids='/media/veracrypt1/MRI/pnTTC/Preproc/test_1sub/39_heudiconv',
-        #path_bids='/media/veracrypt1/MRI/pnTTC/Preproc/37_c1_bids',
-        path_bids='/media/veracrypt1/MRI/pnTTC/Preproc/38_c2_bids',
+        path_bids='/media/veracrypt1/MRI/pnTTC/Preproc/37_c1_bids',
+        #path_bids='/media/veracrypt1/MRI/pnTTC/Preproc/38_c2_bids',
         #path_freesurfer='/media/veracrypt1/MRI/pnTTC/Preproc/test_5sub/pnTTC1_T1_C_FS_10_recon',
         #path_freesurfer='/media/veracrypt1/MRI/pnTTC/Preproc/test_1sub/pnTTC1_T1_C_FS_10_recon',
-        #path_freesurfer='/media/veracrypt1/MRI/pnTTC/pnTTC1_T1_C/FS/12_recon_t1exist',
-        path_freesurfer='/media/veracrypt1/MRI/pnTTC/pnTTC2_T1_C/FS/17_recon',
+        path_freesurfer='/media/veracrypt1/MRI/pnTTC/pnTTC1_T1_C/FS/12_recon_t1exist',
+        #path_freesurfer='/media/veracrypt1/MRI/pnTTC/pnTTC2_T1_C/FS/17_recon',
         #path_out='C:/Users/atiro/Dropbox/Temp/Preproc/test_1sub/40_fieldmap',
         #path_out='/media/veracrypt1/MRI/pnTTC/Preproc/test_5sub/54_prep_fmriprep',
         #path_out='/media/veracrypt1/MRI/pnTTC/Preproc/test_1sub/41_prep_fmriprep',
-        #path_out='/media/veracrypt1/MRI/pnTTC/Preproc/39_c1_1_prep_fmriprep',
-        path_out='/media/veracrypt1/MRI/pnTTC/Preproc/40_c2_1_prep_fmriprep',
+        path_out='/media/veracrypt1/MRI/pnTTC/Preproc/39_c1_1_prep_fmriprep',
+        #path_out='/media/veracrypt1/MRI/pnTTC/Preproc/40_c2_2_prep_fmriprep',
         path_file_fslicense='/usr/local/freesurfer/license.txt',
         #file_id='id_5sub.csv'
         #file_id='id_1sub.csv'
-        #file_id='id_W1_T1QC_new_mild_rsfMRIexist_1.csv'
+        file_id='id_W1_T1QC_new_mild_rsfMRIexist_1.csv'
         #file_id='id_W1_T1QC_new_mild_rsfMRIexist_2.csv'
-        file_id='id_W2_T1QC_new_mild_rsfMRIexist_1.csv'
+        #file_id='id_W2_T1QC_new_mild_rsfMRIexist_1.csv'
         #file_id='id_W2_T1QC_new_mild_rsfMRIexist_2.csv'
         ):
         
