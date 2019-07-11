1b1588c0c1df74acaa51f36dc38b354b9351f32f
Susumu Morita <atirom.umusus@gmail.com>
2019-07-11 09:48:01 +0900

    COCORO double check batch finished

---
+++ spm/batch/batch3_glm_cocoro/batch03_job.m
@@ -0,0 +1,251 @@
+%-----------------------------------------------------------------------
+% Job saved on 08-Jul-2019 23:24:47 by cfg_util (rev $Rev: 6942 $)
+% spm SPM - SPM12 (7219)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.stats.factorial_design.dir = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.factorial_design.des.mreg.scans = '<UNDEFINED>';
+%%
+matlabbatch{1}.spm.stats.factorial_design.des.mreg.mcov(1).c = [-3.1
+                                                                0.5
+                                                                -2.4
+                                                                -2.4
+                                                                7
+                                                                7.7
+                                                                5.3
+                                                                1.7
+                                                                9.1
+                                                                3.9
+                                                                0.9
+                                                                7.2
+                                                                5.9
+                                                                -9.7
+                                                                2.9
+                                                                7.9
+                                                                8.2
+                                                                8.3
+                                                                28.3
+                                                                10.6
+                                                                14.9
+                                                                19.2
+                                                                11.1
+                                                                14.9
+                                                                13.2
+                                                                12.7
+                                                                -14.4
+                                                                20
+                                                                15.6
+                                                                21.8
+                                                                57.2
+                                                                31.8
+                                                                17.6
+                                                                31.1
+                                                                13.7
+                                                                21.3
+                                                                -15.2
+                                                                32.4
+                                                                28.7
+                                                                12.2
+                                                                26.4
+                                                                40.2
+                                                                23.7
+                                                                10
+                                                                13.2
+                                                                -21.6
+                                                                -10.8
+                                                                20.7
+                                                                31.7
+                                                                26.3
+                                                                20.6
+                                                                12.4
+                                                                33.6
+                                                                29.4
+                                                                25.8
+                                                                23.3
+                                                                18.9
+                                                                21.5
+                                                                22.1
+                                                                23.8
+                                                                13
+                                                                13.7
+                                                                30.3
+                                                                17
+                                                                42.9];
+%%
+matlabbatch{1}.spm.stats.factorial_design.des.mreg.mcov(1).cname = 'IQ decline';
+matlabbatch{1}.spm.stats.factorial_design.des.mreg.mcov(1).iCC = 1;
+%%
+matlabbatch{1}.spm.stats.factorial_design.des.mreg.mcov(2).c = [59
+                                                                37
+                                                                36
+                                                                37
+                                                                21
+                                                                20
+                                                                33
+                                                                36
+                                                                39
+                                                                64
+                                                                18
+                                                                36
+                                                                22
+                                                                54
+                                                                35
+                                                                43
+                                                                24
+                                                                15
+                                                                52
+                                                                18
+                                                                59
+                                                                38
+                                                                41
+                                                                31
+                                                                29
+                                                                42
+                                                                17
+                                                                32
+                                                                35
+                                                                34
+                                                                41
+                                                                34
+                                                                41
+                                                                40
+                                                                26
+                                                                42
+                                                                24
+                                                                19
+                                                                36
+                                                                45
+                                                                35
+                                                                23
+                                                                30
+                                                                22
+                                                                27
+                                                                48
+                                                                31
+                                                                20
+                                                                42
+                                                                29
+                                                                29
+                                                                20
+                                                                29
+                                                                31
+                                                                26
+                                                                16
+                                                                27
+                                                                45
+                                                                25
+                                                                54
+                                                                25
+                                                                30
+                                                                41
+                                                                42
+                                                                25];
+%%
+matlabbatch{1}.spm.stats.factorial_design.des.mreg.mcov(2).cname = 'Age';
+matlabbatch{1}.spm.stats.factorial_design.des.mreg.mcov(2).iCC = 1;
+%%
+matlabbatch{1}.spm.stats.factorial_design.des.mreg.mcov(3).c = [2
+                                                                1
+                                                                1
+                                                                1
+                                                                2
+                                                                1
+                                                                1
+                                                                1
+                                                                1
+                                                                2
+                                                                2
+                                                                2
+                                                                2
+                                                                2
+                                                                2
+                                                                1
+                                                                1
+                                                                2
+                                                                2
+                                                                1
+                                                                2
+                                                                2
+                                                                1
+                                                                1
+                                                                2
+                                                                1
+                                                                2
+                                                                1
+                                                                1
+                                                                1
+                                                                1
+                                                                1
+                                                                1
+                                                                2
+                                                                1
+                                                                1
+                                                                2
+                                                                2
+                                                                1
+                                                                2
+                                                                2
+                                                                2
+                                                                1
+                                                                2
+                                                                2
+                                                                2
+                                                                1
+                                                                1
+                                                                2
+                                                                1
+                                                                2
+                                                                2
+                                                                2
+                                                                1
+                                                                2
+                                                                2
+                                                                1
+                                                                2
+                                                                2
+                                                                2
+                                                                2
+                                                                1
+                                                                2
+                                                                1
+                                                                2];
+%%
+matlabbatch{1}.spm.stats.factorial_design.des.mreg.mcov(3).cname = 'Sex';
+matlabbatch{1}.spm.stats.factorial_design.des.mreg.mcov(3).iCC = 5;
+matlabbatch{1}.spm.stats.factorial_design.des.mreg.incint = 1;
+matlabbatch{1}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.masking.tm.tm_none = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.im = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.em = {''};
+matlabbatch{1}.spm.stats.factorial_design.globalc.g_omit = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.glonorm = 1;
+matlabbatch{2}.spm.stats.fmri_est.spmmat(1) = cfg_dep('Factorial design specification: SPM.mat File', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{2}.spm.stats.fmri_est.write_residuals = 0;
+matlabbatch{2}.spm.stats.fmri_est.method.Classical = 1;
+matlabbatch{3}.spm.stats.con.spmmat(1) = cfg_dep('Model estimation: SPM.mat File', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.name = 'positive effect of IQ decline';
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.weights = [0 1 0 0];
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.consess{2}.tcon.name = 'negative effect of IQ decline';
+matlabbatch{3}.spm.stats.con.consess{2}.tcon.weights = [0 -1 0 0];
+matlabbatch{3}.spm.stats.con.consess{2}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.delete = 0;
+matlabbatch{4}.spm.stats.results.spmmat(1) = cfg_dep('Contrast Manager: SPM.mat File', substruct('.','val', '{}',{3}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{4}.spm.stats.results.conspec(1).titlestr = '';
+matlabbatch{4}.spm.stats.results.conspec(1).contrasts = 1;
+matlabbatch{4}.spm.stats.results.conspec(1).threshdesc = 'none';
+matlabbatch{4}.spm.stats.results.conspec(1).thresh = 0.001;
+matlabbatch{4}.spm.stats.results.conspec(1).extent = 0;
+matlabbatch{4}.spm.stats.results.conspec(1).conjunction = 1;
+matlabbatch{4}.spm.stats.results.conspec(1).mask.none = 1;
+matlabbatch{4}.spm.stats.results.conspec(2).titlestr = '';
+matlabbatch{4}.spm.stats.results.conspec(2).contrasts = 2;
+matlabbatch{4}.spm.stats.results.conspec(2).threshdesc = 'none';
+matlabbatch{4}.spm.stats.results.conspec(2).thresh = 0.001;
+matlabbatch{4}.spm.stats.results.conspec(2).extent = 0;
+matlabbatch{4}.spm.stats.results.conspec(2).conjunction = 1;
+matlabbatch{4}.spm.stats.results.conspec(2).mask.none = 1;
+matlabbatch{4}.spm.stats.results.units = 1;
+matlabbatch{4}.spm.stats.results.export{1}.ps = true;
