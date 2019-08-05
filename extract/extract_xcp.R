8d6049aa1b51edfdd3f4f8cf680229b79db84b7e
Unknown <atirom.umusus@gmail.com>
2019-08-05 15:57:43 +0900

    param

--- extract/extract_xcp.R
+++ extract/extract_xcp.R
@@ -17,11 +17,11 @@
 #path_out <- "/media/veracrypt3/MRI_img/pnTTC/preproc"
 
 path_in <- "/media/veracrypt1/MRI_img/pnTTC/preproc"
-path_out <- "/media/veracrypt3/MRI_img/pnTTC/preproc"
+path_out <- "/media/veracrypt2/MRI_img/pnTTC/preproc"
 
-dir_in <-"171_c1_xcp_acompcor"
-dir_out<-"175_c1_ts_acompcor"
-ses<-'ses-01'
+#dir_in <-"171_c1_xcp_acompcor"
+#dir_out<-"175_c1_ts_acompcor"
+#ses<-'ses-01'
 
 #dir_in <-"172_c2_xcp_acompcor"
 #dir_out<-"176_c2_ts_acompcor"
@@ -31,9 +31,9 @@ ses<-'ses-01'
 #dir_out<-"85_c1_ts_aroma"
 #ses<-'ses-01'
 
-#dir_in <-"82_c2_xcp_aroma"
-#dir_out<-"86_c2_ts_aroma"
-#ses<-'ses-02'
+dir_in <-"182_c2_xcp_aroma"
+dir_out<-"186_c2_ts_aroma"
+ses<-'ses-02'
 
 #dir_in <-"91_c1_xcp_36p"
 #dir_out<-"95_c1_ts_36p"
