e463a98f1b2304a6ccd313ed417c3a336c9205c2
Unknown <atirom.umusus@gmail.com>
2019-08-10 19:27:55 +0900

    param

--- extract/extract_xcp.R
+++ extract/extract_xcp.R
@@ -17,7 +17,7 @@
 #path_out <- "/media/veracrypt3/MRI_img/pnTTC/preproc"
 
 path_in <- "/media/veracrypt1/MRI_img/pnTTC/preproc"
-path_out <- "/media/veracrypt2/MRI_img/pnTTC/preproc"
+path_out <- "/media/veracrypt1/MRI_img/pnTTC/preproc"
 
 #dir_in <-"171_c1_xcp_acompcor"
 #dir_out<-"175_c1_ts_acompcor"
@@ -31,17 +31,17 @@ path_out <- "/media/veracrypt2/MRI_img/pnTTC/preproc"
 #dir_out<-"85_c1_ts_aroma"
 #ses<-'ses-01'
 
-dir_in <-"182_c2_xcp_aroma"
-dir_out<-"186_c2_ts_aroma"
-ses<-'ses-02'
+#dir_in <-"182_c2_xcp_aroma"
+#dir_out<-"186_c2_ts_aroma"
+#ses<-'ses-02'
 
 #dir_in <-"91_c1_xcp_36p"
 #dir_out<-"95_c1_ts_36p"
 #ses<-'ses-01'
 
-#dir_in <-"92_c2_xcp_36p"
-#dir_out<-"96_c2_ts_36p"
-#ses<-'ses-02'
+dir_in <-"192_c2_xcp_36p"
+dir_out<-"196_c2_ts_36p"
+ses<-'ses-02'
 
 list_atlas<-c("aal116","glasser360","gordon333","power264","schaefer100","schaefer200","schaefer400")
 #list_atlas<-c("aal116")
