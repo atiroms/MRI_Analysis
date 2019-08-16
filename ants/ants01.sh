4204f0f18c2ae3250ba3aa41e68875838b54cbe8
atiroms <atirom.umusus@gmail.com>
2019-08-16 10:50:00 +0900

    antsRegistrationSyn used instead of -Quick

--- ants/ants01.sh
+++ ants/ants01.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+source activate neuroimaging
 dim=3 # image dimensionality
 AP=/home/atiroms/ANTs/bin/ # /home/yourself/code/ANTS/bin/bin/  # path to ANTs binaries
 DATAPATH=/home/atiroms/Dropbox/temp/Shen_to_ICBM/image_transform/ants_syn01/source/
-OUTPUTPATH=/home/atiroms/Dropbox/temp/Shen_to_ICBM/image_transform/ants_syn01/results/
+OUTPUTPATH=/home/atiroms/Dropbox/temp/Shen_to_ICBM/image_transform/ants_syn01/results2/
 cd ${OUTPUTPATH}
 ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=20  # controls multi-threading
 export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS
 prefix=synquick_
 
-antsRegistrationSyNQuick.sh \
+#antsRegistrationSyNQuick.sh \
+antsRegistrationSyN.sh \
   -d 3 \
   -m ${DATAPATH}colin27_t1_tal_hires.nii \
   -f ${DATAPATH}mni_icbm152_t1_tal_nlin_asym_09c.nii \
