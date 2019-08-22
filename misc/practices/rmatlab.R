292446df57c7d8af0cb03e3477cd0ae2c2a76f2a
Susumu Morita <atirom.umusus@gmail.com>
2019-08-22 18:39:25 +0900

    rmatlab finished

--- misc/practices/rmatlab.R
+++ misc/practices/rmatlab.R
@@ -1,8 +1,11 @@
 
-path<-"C:/Users/atiro/Dropbox/temp/prep_graphvar"
+#path<-"C:/Users/atiro/Dropbox/temp/prep_graphvar"
+path<-"C:/Users/NICT_WS/Dropbox/temp/prep_graphvar"
 
 library(R.matlab)
 
-df<-data.frame(col1=c(1,2),col2=c(3,4))
+#df<-data.frame(col1=c(1,2),col2=c(3,4))
 
-writeMat(file.path(path,"out.mat"),dataframe=df)
+mat_out<-matrix(1:9,nrow=3,ncol=3)
+
+writeMat(file.path(path,"out.mat"),matrix=mat_out)
