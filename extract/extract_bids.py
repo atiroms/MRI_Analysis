52c2812f5f2e8f2f67275f34b6ebdf1e3ab51f0b
Unknown <atirom.umusus@gmail.com>
2019-11-25 00:19:51 +0900

    SubsetNiigz in progress

--- extract/extract_bids.py
+++ extract/extract_bids.py
@@ -68,3 +68,50 @@ class Bids2dir():
             for file_src in tqdm(list_file_src):
                 shutil.copy(file_src,dir_dst)
         print('Finished Bids2dir()')
+
+##################################################
+# Subset and unzip .nii.gz, create clinical data
+##################################################
+
+class SubsetNiigz():
+    def __init__(self,
+        path_src='D:/MRI_img/pnTTC/data/400_niigz',
+        path_dst='D:/MRI_img/pnTTC/data/401_nii',
+        #file_clin='D:/atiroms/Dropbox/MRI_img/pnTTC/puberty/common/CSUB.csv',
+        crt_subset=[[1,['W1_T1QC',1],['W1_rsfMRIexist',1]],
+                    [1,['W2_T1QC',1],['W2_rsfMRIexist',1]]],
+        list_subdir_dst=['ses-01/anat','ses-01/func','ses-02/anat','ses-02/func']):
+        
+        print('Starting SubsetNiigz().')
+
+        # Create output folder
+        print('Starting to create output folder.')
+        list_path_mkdir=[]
+        list_path_mkdir.append(path_dst)
+        list_path_mkdir.append(os.path.join(path_dst,'output'))
+        for subdir in list_subdir_dst:
+            list_path_mkdir.append(os.path.join(path_dst,'output',subdir))
+        for p in list_path_mkdir:
+            if not os.path.exists(p):
+                os.makedirs(p)
+        print('Finished creating output folder.')
+
+        # Copy log folder
+        print('Starting to copy log folder.')
+        path_log_src=os.path.join(path_src,'log')
+        path_log_dst=os.path.join(path_dst,'log')
+        shutil.copytree(path_log_src,path_log_dst)
+        print('Finished copying log folder.')
+
+        # Create longitudinal clinical data
+        df_clin=pd.read_csv(file_clin,encoding = 'unicode_escape')
+        df_clin_long=pd.DataFrame()
+        for list_crt_subset in list_list_crt_subset:
+            df_clin_ses=df_clin.copy()
+            df_clin_ses.insert(0,'ses',list_crt_subset[0])
+            for crt_subset in list_crt_subset[1:]:
+                df_clin_ses=df_clin_ses[df_clin_ses[crt_subset[0]]==crt_subset[1]]
+            df_clin_long=pd.concat([df_clin_long,df_clin_ses])
+        df_clin_long=df_clin_long.reset_index()
+        df_clin_long.to_csv(os.path.join(path_dst,'output','df_clin.csv'))
+
