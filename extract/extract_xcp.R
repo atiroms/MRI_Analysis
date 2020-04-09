dff7b0f65070899edb81fc131c75b74f7bfb027b
Unknown <atirom.umusus@gmail.com>
2020-04-09 23:38:59 +0900

    param

--- extract/extract_xcp.R
+++ extract/extract_xcp.R
@@ -11,12 +11,12 @@
 #**************************************************
 
 #path_in <- "/media/veracrypt2/MRI_img/pnTTC/preproc"
-#path_in <- "/media/atiroms/SSD_2TB/MRI_img/pnTTC/preproc"
-path_in <- "I:/MRI_img/pnTTC/preproc"
+path_in <- "/media/atiroms/SSD_2TB/MRI_img/pnTTC/preproc"
+#path_in <- "I:/MRI_img/pnTTC/preproc"
 
 #path_out <- "/media/veracrypt2/MRI_img/pnTTC/preproc"
-#path_out <- "/media/atiroms/SSD_2TB/MRI_img/pnTTC/preproc"
-path_out <- "I:/MRI_img/pnTTC/preproc"
+path_out <- "/media/atiroms/SSD_2TB/MRI_img/pnTTC/preproc"
+#path_out <- "I:/MRI_img/pnTTC/preproc"
 #path_out<- '/media/atiroms/MORITA_HDD8/MRI_img/pnTTC/preproc'
 
 #dir_in <-"433_c1_xcp_acompcor"
@@ -35,13 +35,13 @@ path_out <- "I:/MRI_img/pnTTC/preproc"
 #dir_out<-"416_c2_ts_acompcor_gsr"
 #ses<-'ses-02'
 
-#dir_in <-"407_c1_xcp_aroma"
-#dir_out<-"417_c1_ts_aroma"
-#ses<-'ses-01'
+dir_in <-"407_c1_xcp_aroma"
+dir_out<-"417_c1_ts_aroma"
+ses<-'ses-01'
 
-dir_in <-"408_c2_xcp_aroma"
-dir_out<-"418_c2_ts_aroma"
-ses<-'ses-02'
+#dir_in <-"408_c2_xcp_aroma"
+#dir_out<-"418_c2_ts_aroma"
+#ses<-'ses-02'
 
 #dir_in <-"409_c1_xcp_aroma_gsr"
 #dir_out<-"419_c1_ts_aroma_gsr"
