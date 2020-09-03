17d7dcbe040495c915a7166abe1287c08fb850be
Susumu Morita <atirom.umusus@gmail.com>
2020-09-03 18:28:58 +0900

    param

--- analyze/connection.R
+++ analyze/connection.R
@@ -12,7 +12,10 @@
 path_exp <- "Dropbox/MRI_img/pnTTC/puberty/stats/func_XCP"
 path_exp_full<-NULL
 
-dir_in<-dir_out<-"401_fc_acompcor"
+
+#dir_in<-dir_out<-"401_fc_acompcor"
+dir_in<-"401_fc_acompcor"
+dir_out<-"402_fp_acompcor"
 list_atlas<-c("aal116","glasser360","gordon333","power264",
               "schaefer100x7","schaefer200x7","schaefer400x7",
               "schaefer100x17","schaefer200x17","schaefer400x17",
