886ba03fb64076c695a0f30a8bba356325789119
Unknown <atirom.umusus@gmail.com>
2020-09-05 10:48:27 +0900

    param

--- analyze/connection.R
+++ analyze/connection.R
@@ -14,13 +14,13 @@ path_exp_full<-NULL
 
 
 #dir_in<-dir_out<-"401_fc_acompcor"
-dir_in<-"411_fc_acompcor_gsr"
-dir_out<-"412_fp_acompcor_gsr"
+dir_in<-"421_fc_aroma"
+dir_out<-"422_fp_aroma"
 list_atlas<-c("aal116","glasser360","gordon333","power264",
               "schaefer100x7","schaefer200x7","schaefer400x7",
               "schaefer100x17","schaefer200x17","schaefer400x17",
               "shen268")
-#path_exp_full<-"/media/atiroms/HDD_05/MRI_img/pnTTC/puberty/stats/func_XCP"
+path_exp_full<-"/media/atiroms/HDD_04/MRI_img/pnTTC/puberty/stats/func_XCP"
 
 #dir_in<-"421_fc_aroma"
 #dir_out<-"427_fc_gamm_aroma"
