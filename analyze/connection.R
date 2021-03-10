4966c7217774da4b5d7c391c891451be59d088b6
atiroms <atirom.umusus@gmail.com>
2021-03-10 17:53:41 +0900

    param

--- analyze/connection.R
+++ analyze/connection.R
@@ -14,7 +14,7 @@ path_exp_full<-NULL
 #path_exp_full<-"/media/atiroms/SSD_02/MRI_img/pnTTC/puberty/stats/func_XCP"
 
 dir_in<-"421_fc_aroma"
-dir_out<-"423.2_fc_gam_cs_aroma_test3" 
+dir_out<-"423.2_fc_gam_cs_aroma_test4" 
 #dir_out<-"424_fc_gamm_aroma_test2"
 #dir_out<-"424_fc_gamm_aroma_test3" # on Ubuntu_1
 #dir_out<-"424_fc_gamm_aroma_test4" # on Ubuntu_2
@@ -22,10 +22,11 @@ dir_out<-"423.2_fc_gam_cs_aroma_test3"
 #list_atlas<-c("aal116","gordon333","ho112","power264",
 #              "schaefer100x17","schaefer200x17","schaefer400x17",
 #              "shen268")
-#list_atlas<-c("aal116")
-#list_atlas<-c("ho112")
-list_atlas<-c("aal116","ho112","power264")
+#list_atlas<-"aal116"
+#list_atlas<-"ho112"
 #list_atlas<-"power264"
+#list_atlas<-c("aal116","ho112")
+list_atlas<-c("aal116","ho112","power264")
 #list_atlas<-c("aal116","glasser360","gordon333","power264",
 #              "schaefer100x7","schaefer200x7","schaefer400x7",
 #              "schaefer100x17","schaefer200x17","schaefer400x17",
