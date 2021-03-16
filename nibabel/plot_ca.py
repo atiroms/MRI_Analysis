a9e206706525bb4b2b1a29e867744be11a0e4e03
atiroms <atirom.umusus@gmail.com>
2021-03-16 22:21:32 +0900

    plot_ca.py done

--- nibabel/plot_ca.py
+++ nibabel/plot_ca.py
@@ -10,17 +10,20 @@ from matplotlib import gridspec
 
 #path_atlas='D:/atiro/Dropbox/MRI_img/pnTTC/template_atlas/XCP_atlas_plot'
 #file_roi='D:/atiro/Dropbox/MRI_img/pnTTC/puberty/common/ROI.csv'
-path_atlas='D:/NICT_WS/Dropbox/MRI_img/pnTTC/template_atlas/XCP_atlas_plot'
-path_exp='D:/NICT_WS/Dropbox/MRI_img/pnTTC/puberty/stats/func_XCP'
+path_atlas='D:/atiro/Dropbox/MRI_img/pnTTC/template_atlas/XCP_atlas_plot'
+path_exp='D:/atiro/Dropbox/MRI_img/pnTTC/puberty/stats/func_XCP'
 dir_in='425_fc_ca_aroma'
-[atlas,variable,wave,model,term,sex,p_threshold,id_net]=\
-    ['power264','corti','c1m2','l','hormone',1,0.005,1]
 
-file_roi='D:/NICT_WS/Dropbox/MRI_img/pnTTC/puberty/common/ROI.csv'
+file_roi='D:/atiro/Dropbox/MRI_img/pnTTC/puberty/common/ROI.csv'
 #list_atlas=['aal116','glasser360','gordon333','ho112','power264','schaefer100x7','schaefer100x17','schaefer200x7','schaefer200x17','schaefer400x7','schaefer400x17','shen268']
+list_atlas=['aal116','gordon333','ho112','power264','schaefer100x17','schaefer200x17','schaefer400x17','shen268']
 transparency_roi=0.3
 
-cmap=plt.get_cmap('Wistia')
+#cmap=plt.get_cmap('Wistia')
+#cmap=plt.get_cmap('viridis')
+#cmap=plt.get_cmap('plasma')
+#cmap=plt.get_cmap('cividis')
+cmap=plt.get_cmap('cool')
 
 ####
 
@@ -30,19 +33,69 @@ nii_template=nib.load(file_template)
 arr_template=nii_template.get_fdata()
 arr_template=arr_template/arr_template.max()
 
-df_roi_atlas=df_roi[df_roi['atlas']==atlas]
-file_atlas=os.path.join(path_atlas,atlas+'MNI.nii.gz')
-nii_atlas=nib.load(file_atlas)
-if nii_atlas.shape!=nii_template.shape:
-    nii_atlas=resample_img(nii_atlas,target_affine=nii_template.affine,target_shape=nii_template.shape,interpolation='nearest')
-arr_atlas=nii_atlas.get_fdata()
-
-####
-
 df_strength=pd.read_csv(os.path.join(path_exp,dir_in,'output','result','fc_ca_str.csv'))
-df_node_subnet=df_node.copy()
-df_node_subnet=df_node_subnet[(df_node['atlas']==atlas) & (df_node['variable']==variable)\
-    & (df_node['wave']==wave) & (df_node['model']==model) & (df_node['term']==term)\
-    & (df_node['sex']==sex) & (df_node['p_threshold']==p_threshold)\
-    & (df_node['id_net']==id_net)]
-df_node_subnet['degree_norm']=df_node_subnet['degree']/max(df_node_subnet['degree'])
+
+for atlas in list_atlas:
+    df_roi_atlas=df_roi[df_roi['atlas']==atlas]
+    file_atlas=os.path.join(path_atlas,atlas+'MNI.nii.gz')
+    nii_atlas=nib.load(file_atlas)
+    if nii_atlas.shape!=nii_template.shape:
+        nii_atlas=resample_img(nii_atlas,target_affine=nii_template.affine,target_shape=nii_template.shape,interpolation='nearest')
+    arr_atlas=nii_atlas.get_fdata()
+
+    for wave_mri in [1,2,'2-1']:
+        for method in ['pca','ica']:
+            for sex in ['male','female','both']:
+                for dim in [10,20,40]:
+                    for comp in range(1,dim+1):
+                        df_strength_subset=df_strength.copy()
+                        df_strength_subset=df_strength_subset[(df_strength['atlas']==atlas)\
+                        & (df_strength['wave_mri']==wave_mri) & (df_strength['method']==method)\
+                        & (df_strength['sex']==sex) & (df_strength['dim']==dim)\
+                        & (df_strength['comp']==comp)]
+                        if len(df_strength_subset)>0:
+                            df_strength_subset['strength_norm']=df_strength_subset['strength']/max(df_strength_subset['strength'])
+                            
+                            arr_mask=np.zeros(shape=arr_template.shape)
+                            arr_colored_node=np.zeros(shape=arr_template.shape+tuple([3]))
+
+                            for id_row in range(df_strength_subset.shape[0]):
+                                [id_roi,strength_norm]=df_strength_subset.iloc[id_row][['node','strength_norm']]
+                                intensity_roi=int(df_roi[df_roi['id']==id_roi]['intensity'])
+
+                                arr_roi=np.zeros(shape=arr_template.shape)
+                                arr_roi[arr_atlas==intensity_roi]=1
+                                arr_roi[arr_atlas!=intensity_roi]=0
+
+                                arr_mask=arr_mask+arr_roi
+                                arr_colored_node[arr_atlas==intensity_roi,:]=np.asarray(cmap(strength_norm)[0:3])
+
+                            arr_plot=np.copy(arr_template)
+                            arr_plot[arr_mask==1]=arr_plot[arr_mask==1]*transparency_roi
+                            arr_plot=np.stack([arr_plot,arr_plot,arr_plot],3)
+                            arr_plot=arr_plot+arr_colored_node*(1-transparency_roi)
+
+
+                            gs = gridspec.GridSpec(5, 8,wspace=0.05,hspace=0.05)
+                            #gs = gridspec.GridSpec(5, 8)
+
+                            #gs.update(left=0.1,right=0.1,top=0.5,bottom=0.1,wspace=0.1,hspace=0.1)
+                            fig=plt.figure(figsize=(15,12))
+                            idx_fig=0
+                            for zloc in range(0,80,2):
+                                arr_plot_axial=np.flipud(np.fliplr(arr_plot[:,:,zloc].transpose(1,0,2)))
+                                idx_fig_row=idx_fig // 8
+                                idx_fig_col=idx_fig % 8
+
+                                plt.subplot(gs[idx_fig_row,idx_fig_col])
+                                plt.imshow(arr_plot_axial)
+                                plt.axis('off')
+
+                                idx_fig+=1
+
+                            plt.suptitle('atlas: '+atlas+', method: '+method+', wave: '+str(wave_mri)+', sex: '+sex+', dim: '+str(dim).zfill(3)+', comp: '+str(comp).zfill(3))
+                            plt.tight_layout()
+
+                            filename='atl-'+atlas+'_method-'+method+'_ses-'+str(wave_mri)+'_sex-'+sex+'_dim-'+str(dim).zfill(3)+'_comp-'+str(comp).zfill(3)+'_fc_ca_str.png'
+                            fig.savefig(os.path.join(path_exp,dir_in,'output','plot',filename))
+                                                    
