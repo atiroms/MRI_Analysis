ef0348fbc2954d0a8e532746edf1382fccfdfa90
atiroms <atirom.umusus@gmail.com>
2021-06-02 21:06:47 +0900

    update plot_fc.py

--- nibabel/plot_fc.py
+++ nibabel/plot_fc.py
@@ -11,20 +11,21 @@ from matplotlib.lines import Line2D
 
 ####
 dir_in='424_fc_gamm_aroma_test24'
-[atlas,variable,wave,model,term,sex,sign,p_threshold,id_net]=\
-    ['ho112','adrenal','long','l','tanner.L',2,'both',0.005,1]
+path_parent='D:/NICT_WS/'
+#path_parent='D:/atiro/'
 
 ####
-file_roi='D:/NICT_WS/Dropbox/MRI_img/pnTTC/puberty/common/ROI.csv'
-path_atlas='D:/NICT_WS/Dropbox/MRI_img/pnTTC/template_atlas/XCP_atlas_plot'
-path_exp='D:/NICT_WS/Dropbox/MRI_img/pnTTC/puberty/stats/func_XCP'
+file_roi=path_parent+'Dropbox/MRI_img/pnTTC/puberty/common/ROI.csv'
+path_atlas=path_parent+'Dropbox/MRI_img/pnTTC/template_atlas/XCP_atlas_plot'
+path_exp=path_parent+'Dropbox/MRI_img/pnTTC/puberty/stats/func_XCP'
 df_roi=pd.read_csv(file_roi)
 ####
 
 df_fwep=pd.read_csv(os.path.join(path_exp,dir_in,'output','result','perm_fwep.csv'))
 df_edge=pd.read_csv(os.path.join(path_exp,dir_in,'output','result','bfs_edge.csv'))
 
-os.mkdir(os.path.join(path_exp,dir_in,'output','plot','plot_subnet'))
+if not os.path.exists(os.path.join(path_exp,dir_in,'output','plot','plot_subnet')):
+    os.mkdir(os.path.join(path_exp,dir_in,'output','plot','plot_subnet'))
 
 ####
 # Calculate gravity center
@@ -39,12 +40,12 @@ for atlas in list_atlas:
         df_roi_atlas.loc[idx_row,['gc_x','gc_y','gc_z']]=arr_gc[list_intensity.index(intensity_roi),:]
     dct_df_roi_atlas[atlas]=df_roi_atlas
 
-
 # Iterate over significant networks
-df_fwep_signif=df_fwep.loc[df_fwep['p_fwe']<=0.05,:]
-for idx_fwep in df_fwep_signif.index:
+df_fwep_plot=df_fwep.copy()
+#df_fwep_plot=df_fwep_plot.loc[df_fwep_plot['p_fwe']<=0.05,:]
+for idx_fwep in df_fwep_plot.index:
     [atlas,variable,wave,model,term,sex,sign,p_threshold,id_net]=\
-        df_fwep_signif.loc[idx_fwep,["atlas","variable","wave","model","term","sex","sign","p_threshold","id_net"]]
+        df_fwep_plot.loc[idx_fwep,["atlas","variable","wave","model","term","sex","sign","p_threshold","id_net"]]
 
     # Prepare edge and node dataframe
     df_edge_subnet=df_edge.copy()
@@ -74,7 +75,10 @@ for idx_fwep in df_fwep_signif.index:
         arr_corr[idx_from,idx_to]=arr_corr[idx_to,idx_from]=df_edge_subnet.loc[idx_row,'value']
 
     # Plot
-    arr_color=cm.nipy_spectral(np.linspace(0, 1, len(list_node)))
+    #arr_color=cm.nipy_spectral(np.linspace(0, 1, len(list_node)))
+    np.random.seed(1)
+    arr_color=np.random.rand(len(list_node),4)
+    arr_color[:,3]=1
     if sex==1:
         label_sex='m'
     elif sex==2:
@@ -83,18 +87,17 @@ for idx_fwep in df_fwep_signif.index:
         label_sex='mf'
     fig, axes = plt.subplots(2,1,figsize=(16,16),gridspec_kw={'height_ratios': [1,2]})                       
     display=plotting.plot_connectome(arr_corr, arr_coord,axes=axes[0],colorbar=True,
-                                     edge_cmap=cm.Spectral_r,
+                                     #edge_cmap=cm.Spectral_r,
+                                     edge_cmap=cm.seismic,
                                      node_color=arr_color,edge_kwargs={'alpha':0.7},alpha=0.7)
-    #axes[0].axis("off")
     axes[1].axis("off")
-    #axes[2].axis("off")
+    # dummy lines for legend
     list_legend=[]
     for idx_node in range(len(list_node)):
-        list_legend=list_legend+[Line2D([0], [0], marker='o', color='w', label=df_node.iloc[idx_node]['label'],markerfacecolor=arr_color[idx_node], markersize=5)]
+        list_legend=list_legend+[Line2D([0], [0], marker='o', color='w', label=df_node.iloc[idx_node]['label'],markerfacecolor=arr_color[idx_node], markersize=10)]
     axes[1].legend(handles=list_legend, loc='upper left',bbox_to_anchor=(0, 1),ncol=4,
                fontsize='small',frameon=False)
     txt_title='atlas: '+atlas+', var: '+variable+', wave: '+str(wave)+', mod: '+model+', term: '+term+', sex: '+label_sex+', cdt: '+str(p_threshold)+', sign: '+sign+', #'+str(id_net)
     axes[0].set_title(label=txt_title,loc='center')
     fname='atl-'+atlas+'_var-'+variable+'_wav-'+str(wave)+'_mod-'+model+'_trm-'+term+'_sex-'+label_sex+'_pval-p_'+str(p_threshold)+'_sgn-'+sign+'_idx-'+str(id_net)+'_subnet.png'
     fig.savefig(os.path.join(path_exp,dir_in,'output','plot','plot_subnet',fname))
-    #fig.savefig(os.path.join(path_atlas,atlas,id_roi+'.png'))
